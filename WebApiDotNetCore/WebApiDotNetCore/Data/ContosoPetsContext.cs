2f381f8bf96e6c7ce7fa23a84374151b6bf6af46
Kevser Yolcu <kyolcu@gtu.edu.tr>
2020-03-18 22:28:40 +0300

    webapi .net core app

---
+++ WebApiDotNetCore/WebApiDotNetCore/Data/ContosoPetsContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiDotNetCore.Models;
+
+namespace WebApiDotNetCore.Data
+{
+    public class ContosoPetsContext : DbContext
+    {
+        public ContosoPetsContext(DbContextOptions<ContosoPetsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
